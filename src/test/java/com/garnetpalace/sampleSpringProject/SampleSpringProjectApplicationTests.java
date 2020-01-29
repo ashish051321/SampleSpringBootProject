fd06d803c098c7cae028923f837056166a83632c
ashish051321 <ashish.25jl@gmail.com>
2020-01-29 23:59:17 +0530

    initial commit

---
+++ src/test/java/com/garnetpalace/sampleSpringProject/SampleSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garnetpalace.sampleSpringProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
